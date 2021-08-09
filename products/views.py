e6506f8bd1f47389aaeeae9a833f4089e2bed5a1
raymondkeogh <raymondkeogh@gmail.com>
2021-08-09 17:51:24 +0000

    Add product search functionality

--- products/views.py
+++ products/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product, PhotoForm
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from .models import Product, PhotoForm
 
 
 def upload(request):
+    """ Connects to cloudinary to allow file upload"""
     context = dict(backend_form=PhotoForm())
 
     if request.method == 'POST':
@@ -18,13 +20,26 @@ def upload(request):
 
 def all_products(request):
     """ A view to show all products """
-    products = Product.objects.all().order_by('name') 
+    products = Product.objects.all().order_by('name')
+    query = None
+
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if not query:
+            messages.error(request, "You didn't enter any search criteria!")
+            return redirect(reverse('products'))
+
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
+
     paginator = Paginator(products, 25)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
+
     context = {
         'products': products,
         'page_obj': page_obj,
+        'search': query,
     }
     return render(request, 'products/products.html', context)
 
