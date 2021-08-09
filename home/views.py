7e55d30c075b01e095b2948ba605e046d2b29d40
raymondkeogh <raymondkeogh@gmail.com>
2021-08-09 23:05:26 +0000

    Add carousel template logic and index view

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
+import random
 from django.shortcuts import render
+from products.models import Product
 
-# Create your views here.
 
 def index(request):
     """ A view to return the index page """
+    num_products = Product.objects.all().count()
+    rand_products = random.sample(range(num_products), num_products)
+    sample_products = Product.objects.filter(id__in=rand_products)
 
-    return render(request, 'home/index.html')
+    context = {
+        'random_product': sample_products,
+    }
+    return render(request, 'home/index.html', context)
