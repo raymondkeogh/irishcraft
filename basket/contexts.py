3c2caaa508de5ba04e8284cbf0d0a618391e2139
raymondkeogh <raymondkeogh@gmail.com>
2021-08-14 17:01:25 +0000

    Update context processor

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,15 +1,27 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def basket_contents(request):
     """Context to return basket contents"""
 
     basket_items = []
-
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
 
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+        
     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)    
 
     grand_total = delivery + total
