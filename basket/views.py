9058ebc0f3dfe33a2a76d1b1ed2ff4b46b3528d0
raymondkeogh <raymondkeogh@gmail.com>
2021-08-14 12:54:51 +0000

    Add basket app, views and templates

---
+++ basket/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+def view_basket(request):
+    """A view to show the shopping basket"""
+
+    return render(request, 'basket/basket.html')
--- irishcraft/settings.py
+++ irishcraft/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'cloudinary',
+    'basket',
 ]
 
 MIDDLEWARE = [
