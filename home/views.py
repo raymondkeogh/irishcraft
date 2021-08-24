afa73df95dfdedf98956482dd758140f2fe05b1d
raymondkeogh <raymondkeogh@gmail.com>
2021-08-24 15:50:49 +0000

    Add category featurette template code and view

--- home/views.py
+++ home/views.py
@@ -1,4 +1,3 @@
-import random
 from django.shortcuts import render
 from products.models import Product
 
@@ -7,8 +6,22 @@ def index(request):
     """ A view to return the index page """
     products = Product.objects.all()
     grid_sample_products = Product.objects.order_by('?')[:9]
+
+    glassware_first = Product.objects.filter(
+        category__name="glassware").first()
+    textiles_first = Product.objects.filter(
+        category__name="textiles").first()
+    prints_first = Product.objects.filter(
+        category__name="prints").first()
+    kitchen_first = Product.objects.filter(
+        category__name="kitchen").first()
+
     context = {
         'products': products,
         'grid_sample_products': grid_sample_products,
+        'glassware_first': glassware_first,
+        'textiles_first': textiles_first,
+        'prints_first': prints_first,
+        'kitchen_first': kitchen_first,
     }
     return render(request, 'home/index.html', context)
