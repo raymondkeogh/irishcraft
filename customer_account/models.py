16bcd62afb72e401fa79177230b48459657966e5
raymondkeogh <raymondkeogh@gmail.com>
2021-08-24 15:26:07 +0000

    Bugfix: Superuser basket pre login filled
    Cleared session cookie for superuser on login to prevent a bug where items added to cart pre superuserlogin carry across causing a 404 error when an item in the basket is deleted by super user from the database

--- customer_account/models.py
+++ customer_account/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from allauth.account.signals import (
+    user_signed_up, user_logged_in, password_changed)
 
 from django.contrib.auth.models import User
 
@@ -24,7 +26,6 @@ class CustomerAccount(models.Model):
     country = models.CharField(
         max_length=40, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
-    email = models.EmailField(max_length=70, blank=True, unique=True)
 
     def __str__(self):
         return self.user.username
@@ -39,3 +40,13 @@ def create_or_update_customer_account(sender, instance, created, **kwargs):
         CustomerAccount.objects.create(user=instance)
 
     instance.customeraccount.save()
+
+
+@receiver(user_logged_in)
+def check_if_superuser(sender, request, user, **kwargs):
+    """
+    Check if user is super user and delete session variable to prevent basket items causing 404 error when deleted from db
+    """
+    if request.user.is_superuser:
+        del request.session['basket']
+        request.session.modified = True
