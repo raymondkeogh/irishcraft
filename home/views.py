d544a4d5bfc659d48f6e34eb21bb16db84a455ce
raymondkeogh <raymondkeogh@gmail.com>
2021-09-13 06:51:21 +0000

    Bugfix: Add try catch to prevent empty db errors

--- home/views.py
+++ home/views.py
@@ -1,20 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from products.models import Product
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def index(request):
     """ A view to return the index page """
-    products = Product.objects.all()
-    grid_sample_products = Product.objects.order_by('?')[:9]
+    try:
+        products = Product.objects.all()
+        grid_sample_products = Product.objects.order_by('?')[:9]
+    except ObjectDoesNotExist:
+        products = None
 
-    glassware_first = Product.objects.filter(
-        category__name="glassware").first()
-    textiles_first = Product.objects.filter(
-        category__name="textiles").first()
-    prints_first = Product.objects.filter(
-        category__name="prints").first()
-    kitchen_first = Product.objects.filter(
-        category__name="kitchen").first()
+    try:
+        glassware_first = Product.objects.filter(
+            category__name="glassware").first()
+    except ObjectDoesNotExist:
+        glassware_first = None
+
+    try:
+        textiles_first = Product.objects.filter(
+            category__name="textiles").first()
+    except ObjectDoesNotExist:
+        textiles_first = None
+
+    try:
+        prints_first = Product.objects.filter(
+            category__name="prints").first()
+    except ObjectDoesNotExist:
+        prints_first = None
+
+    try:
+        kitchen_first = Product.objects.filter(
+            category__name="kitchen").first()
+    except ObjectDoesNotExist:
+        kitchen_first = None
 
     context = {
         'products': products,
