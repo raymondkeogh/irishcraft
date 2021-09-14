2b06488c0034714c87824b623e2793e71d14fcd1
raymondkeogh <raymondkeogh@gmail.com>
2021-09-14 18:53:52 +0000

    Update checkout form

--- irishcraft/settings.py
+++ irishcraft/settings.py
@@ -135,10 +135,10 @@ WSGI_APPLICATION = 'irishcraft.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-if 'DATABASE_URL' in os.environ:
+if 'DATABASE_URL' not in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+    }
 else:
     DATABASES = {
         'default': {
