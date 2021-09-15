bf8c247b7170c20ab70acefdab64bcae514e3129
raymondkeogh <raymondkeogh@gmail.com>
2021-09-15 18:46:54 +0000

    Bugfix:SuspiciousOperation - removed backslash

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -22,7 +22,7 @@ class StripeWH_Handler:
 
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
-        cust_email = order.email
+        print("this is after the email")
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
             {'order': order})
