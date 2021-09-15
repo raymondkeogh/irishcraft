684f2abdfd0139f9cf25e3079959f6c1d28de954
raymondkeogh <raymondkeogh@gmail.com>
2021-09-15 20:37:56 +0000

    Debug: order email not sending, missing send_mail param

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -22,20 +22,20 @@ class StripeWH_Handler:
 
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
-        print("this is after the email")
+        cust_email = order.email
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
             {'order': order})
         body = render_to_string(
             'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-               
+      
         send_mail(
             subject,
             body,
             settings.DEFAULT_FROM_EMAIL,
             [cust_email]
-        )      
+        )
 
 
     def handle_event(self, event):
