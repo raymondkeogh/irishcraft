a2a7730498d818f15daa14af415018d9b3f8da76
raymondkeogh <raymondkeogh@gmail.com>
2021-09-18 16:46:31 +0000

    Adjust CSS for images and remove 1 carousel

--- irishcraft/settings.py
+++ irishcraft/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
-import environ
 import os
+import environ
+
 import dj_database_url
 import cloudinary
 import cloudinary.uploader
@@ -231,9 +231,9 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 # Stripe
 
 STRIPE_CURRENCY = 'eur'
-STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY", "")
-STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY", "")
-STRIPE_WH_SECRET = os.environ.get("STRIPE_WH_SECRET", "")
+STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY", "")
+STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
+STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
 
 # if 'DEVELOPMENT' in os.environ:
 #     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -243,6 +243,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASS')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,2 @@
   var myCarousel = document.querySelector('#myCarousel');
   var carousel = new bootstrap.Carousel(myCarousel);
-  var myCarousel2 = document.querySelector('#myCarousel2');
-  var carousel2 = new bootstrap.Carousel(myCarousel2);
-
-
-  
