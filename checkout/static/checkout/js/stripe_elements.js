2419b341a306bd617271150d8b361fc431cf2088
raymondkeogh <raymondkeogh@gmail.com>
2021-10-02 19:49:16 +0000

    Firefox, Safari and IE style adjustments

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -5,18 +5,20 @@ var elements = stripe.elements();
 var style = {
     base: {
         color: '#000',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontFamily: '"Work Sans", sans-serif',
         fontSmoothing: 'antialiased',
-        fontSize: '16px',
+        fontSize: '20px',
         '::placeholder': {
             color: '#aab7c4'
-        }
+        },
+        fontWeight: '400',
     },
     invalid: {
         color: '#dc3545',
         iconColor: '#dc3545'
     }
 };
+
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
