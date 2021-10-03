fe077a050f753a03a00cf77385b7709c3c8a2622
raymondkeogh <raymondkeogh@gmail.com>
2021-10-03 11:49:17 +0000

    JSLint validation

--- static/js/dropdown_basket.js
+++ static/js/dropdown_basket.js
@@ -1,10 +1,10 @@
 // Basket dropdown hide/show script
 $("#menu-icon").click(function (e) {
 e.stopPropagation();
-$('ul.box').toggle();
+$("ul.box").toggle();
 });
-$('body').click(function () {
-$('ul.box').hide();
+$("body").click(function () {
+$("ul.box").hide();
 });
 
 // credit: https://stackoverflow.com/questions/5811122/how-to-trigger-a-click-on-a-link-using-jquery
