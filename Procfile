27640baa201dad4bebf4d546eb34d516542b7449
raymondkeogh <raymondkeogh@gmail.com>
2022-11-20 22:18:18 +0000

    Create build.sh and add Render host

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn irishcraft.wsgi:application
--- irishcraft/settings.py
+++ irishcraft/settings.py
@@ -38,6 +38,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['irishcraft.herokuapp.com', 'localhost', '127.0.0.1']
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Application definition
 
 INSTALLED_APPS = [
